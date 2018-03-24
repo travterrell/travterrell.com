b14404ef57cf6cd459c3bd8bc873a43ab15bfb7a
Travis Terrell <travterrell@gmail.com>
2018-03-24 14:27:11 -0700

    Added metadeta. Edited background image

--- about/index.php
+++ about/index.php
@@ -5,12 +5,15 @@
     <title>TravTerrell.com</title>
     <link rel="icon" type="image/png" href="../assets/images/travicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="HTML,CSS,JavaScript,SQL,PHP,jQuery,DOM,Bootstrap,Web development,references,examples,source code,adobe creative suite, photoshop, illustrator">
+    <meta name="description" content="Travis Terrell - Full Stack Web Development and Graphic Design using HTML, CSS, JavaScript, jQuery, PHP, MySQL, Adobe Creative Suite, and more!">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../styles.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!--[if lt IE 9]>
-      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>  
     <![endif]-->
   </head>
 
